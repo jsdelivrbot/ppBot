a075f3c0f03984b7b63e4d854f1395d8eb4b43f2
marcodagolini <marco.dagolini@gmail.com>
2018-02-07 00:39:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var skill = "321803613";
 
 
 var app = express();
-
+app.listen();
 app.set('port', (process.env.PORT || 5000));
 
 // Required to allow access to the service across different domains
